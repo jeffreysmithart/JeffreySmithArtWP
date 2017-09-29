6bca5fb0f505d829047e36ee4f00b59c3bdb56c6
Jeffrey Smith <jsmith@Jeffreys-MacBook-Pro.local>
2017-09-29 07:58:21 -0500

    update to related posts component. Styling fix.

--- templates/related-posts.php
+++ templates/related-posts.php
@@ -7,8 +7,10 @@ if (is_singular('portfolio')){
     $tags = wp_get_post_tags($post->ID);
 }
 if ($tags) {
-    echo '<h3 class="section-headline constrain">Related </h3>';
+
     $first_tag = $tags[0]->term_id;
+    $first_tag_obj = get_term( $first_tag);
+    $first_tag_name = $first_tag_obj->slug;
     if (is_singular('portfolio')){
 
         $args=array(
@@ -37,12 +39,14 @@ if ($tags) {
 
     $my_query = new WP_Query($args);
     if( $my_query->have_posts() ) {
+        echo '<h3 class="section-headline constrain">Related </h3>';
         echo '<div class="related-posts constrain">';
         while ($my_query->have_posts()) : $my_query->the_post(); ?>
             <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
                 <div class="related-posts__post" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
                 <div class="related-posts__text-content">
-                <h2><?php the_title(); ?></h2>
+                    <div class="post-category"><?php echo $first_tag_name; ?></div>
+                    <h2><?php the_title(); ?></h2>
                 </div>
             </div>
             </a>
